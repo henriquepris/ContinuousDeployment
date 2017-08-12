84526a6b6484f8fe13a643f00f9eed53b6f3e86d
Henrique Rocha <henrique.rocha@pris.com.br>
2017-08-12 13:49:14 -0300

    Retirada de comentários

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -15,8 +15,8 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult About()
         {
-            //ViewData["Message"] = "Your application description page.";
-            //return View();
+            ViewData["Message"] = "Your application description page.";
+            return View();
         }
 
         public IActionResult Contact()
